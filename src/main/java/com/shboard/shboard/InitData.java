70a8e1071c50b256ce8c5910ce6bab6ad6a7a3b9
sh111-coder <ohk9134@naver.com>
2024-02-15 16:43:59 +0900

    test: 테스트용 yml & 테스트용 더미데이터 추가

--- src/main/java/com/shboard/shboard/InitData.java
+++ src/main/java/com/shboard/shboard/InitData.java
@@ -1,6 +1,7 @@
 package com.shboard.shboard;
 
 import com.shboard.shboard.board.domain.Board;
+import com.shboard.shboard.global.session.domain.MemberSession;
 import com.shboard.shboard.member.domain.Member;
 import jakarta.annotation.PostConstruct;
 import jakarta.persistence.EntityManager;
@@ -10,7 +11,7 @@ import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
-@Profile({"local", "test"})
+@Profile("session-test")
 @Component
 @RequiredArgsConstructor
 public class InitData {
@@ -29,17 +30,21 @@ public class InitData {
 
         @Transactional
         public void init() {
-            int totalPostCount = 10;
-
-            final Member member = com.shboard.shboard.member.domain.Member.builder()
-                    .loginId("sh111")
-                    .password("password1!")
-                    .nickname("성하")
-                    .build();
-            em.persist(member);
+            int totalPostCount = 100;
 
             for (int i = 1; i <= totalPostCount; i++) {
-                final Board board = new Board(em.find(Member.class, 1L), "title" + i, "content" + i);
+                final String loginId = "seongha" + i;
+                final Member member = com.shboard.shboard.member.domain.Member.builder()
+                        .loginId(loginId)
+                        .password("password1!")
+                        .nickname("sh" + i)
+                        .build();
+                em.persist(member);
+
+                final MemberSession memberSession = new MemberSession("session" + i, loginId);
+                em.persist(memberSession);
+
+                final Board board = new Board(em.find(Member.class, i), "title" + i, "content" + i);
                 em.persist(board);
             }
         }
